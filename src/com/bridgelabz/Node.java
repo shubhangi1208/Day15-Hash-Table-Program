fab82b16c074a45ebfa183c4a07cf88fa0cf3c47
shubhangi1208 <ashubhangi1996@gmail.com>
2022-01-22 20:00:22 +0530

    ADDED:User can find frequency of words in a sentences

--- src/com/bridgelabz/Node.java
+++ src/com/bridgelabz/Node.java
@@ -1,12 +1,12 @@
 package com.bridgelabz;
 
-class Nodes <T extends Comparable<T>> {
+class Node <T extends Comparable<T>> {
     public T key;
     public int value;
     public Node next;
 
     //Node Constructor for Creating New Node
-    Nodes(T key) {
+    Node(T key) {
         this.key = key;
         this.value = 1;
         this.next = null;
